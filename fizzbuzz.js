49ca8abb178baa5c4e5e84e030d2073b68aa8751
JudgeFudge19 <dremukare@gmail.com>
2021-10-01 14:27:08 +0300

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,28 @@
-const fizzBuzz = (n, array_m) => {
-	if (n && array_m) {
-		for (let i = 0; i < n; i++) {
-			if (!Number.isInteger(array_m[i])) array_m[i] = Number(array_m[i]);
-			if (array_m[i] % 3 === 0 && array_m[i] % 5 === 0) console.log('FizzBuzz');
-			else if (array_m[i] % 3 === 0) console.log('Fizz');
-			else if (array_m[i] % 5 === 0) console.log('Buzz');
-			else console.log('N/A');
-		}
-	} else {
-		console.log('Please enter a valid value');
+const fizzBuzz = (m) => {
+	// if (n && m && n >= 1 && n <= 100 && m >= 1 && m <= 100) {
+	// if (m && m >= 1 && m <= 100) {
+	const arrayOfNums = [];
+	let str = '';
+	// for (let i = 0; i < n; i++) {
+	for (let j = 0; j <= m; j++) {
+		arrayOfNums.push(j);
 	}
+	arrayOfNums.shift();
+	for (let i = 0; i < arrayOfNums.length; i++) {
+		if (arrayOfNums[i] % 3 === 0 && arrayOfNums[i] % 5 === 0)
+			str += 'FizzBuzz ';
+		else if (arrayOfNums[i] % 3 === 0) str += 'Fizz ';
+		else if (arrayOfNums[i] % 5 === 0) str += 'Buzz ';
+		else str += 'N/A ';
+	}
+	console.log(str);
+	// }
+	// } else {
+	// 	console.log('Please enter a valid value');
+	// }
 };
 
-fizzBuzz(4, [1, 4, 10, 15]);
+fizzBuzz(1);
+fizzBuzz(4);
+fizzBuzz(10);
+fizzBuzz(15);
